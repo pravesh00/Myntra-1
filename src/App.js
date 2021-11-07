c78d80b8ed0725e8455f0d258292994d82380083
Akshit Goyal <akshit2392@gmail.com>
2021-11-07 11:23:16 +0530

    commit after routing

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import Navbar from './components/navbar';
 import Titlebar from './components/titlebar';
 import Home from './components/home';
 import Sizeprofile from './components/sizeprofile';
-import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 
 function App() {
   return (
     <>
       <Router>
-      <Navbar />
-      <h3 id="h1">Clothes - 50 items</h3>
-      <Titlebar />
+        <Navbar />
+        <h3 id="h1">Clothes - 50 items</h3>
+        <Titlebar />
         <Switch>
           <Route exact path="/">
             <Home />
