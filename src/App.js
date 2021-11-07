f7214d328431e80c8c350f43742bfbae7c165f2c
Akshit Goyal <akshit2392@gmail.com>
2021-11-07 15:36:19 +0530

    thoda ignore karna hai pravesh

--- src/App.js
+++ src/App.js
@@ -3,27 +3,28 @@ import Navbar from './components/navbar';
 import Titlebar from './components/titlebar';
 import Home from './components/home';
 import Sizeprofile from './components/sizeprofile';
+import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 
-function App() {
-  return (
-    <>
-      <Router>
-        <Navbar />
-        <h3 id="h1">Clothes - 50 items</h3>
-        <Titlebar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/sizeprofile">
-            <Sizeprofile />
-          </Route>
-        </Switch>
-      </Router>
-    </>
-  );
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <Router>
+                    <Navbar />
+                    <h3 id="h1">Clothes - 50 items</h3>
+                    <Titlebar />
+                    <Switch>
+                        <Route exact path="/">
+                            <Home />
+                        </Route>
+                        <Route exact path="/sizeprofile">
+                            <Sizeprofile />
+                        </Route>
+                    </Switch>
+                </Router>
+            </div>
+        )
+    }
 }
-
-export default App;
